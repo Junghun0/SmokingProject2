8d2469b2714b951e483fb2fd949ae943c9a9b41f
parkjunghun <go9018@naver.com>
2017-09-16 14:14:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/parkjunghun/smokingproject2/EmptyFragment.java
@@ -0,0 +1,26 @@
+package com.example.parkjunghun.smokingproject2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by parkjunghun on 2017. 9. 16..
+ */
+
+public class EmptyFragment extends Fragment {
+
+    public EmptyFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_empty, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/parkjunghun/smokingproject2/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.parkjunghun.smokingproject2;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction fragmentTrasaction = fm.beginTransaction();
+        fragmentTrasaction.replace(R.id.fragment, new EmptyFragment());
+        fragmentTrasaction.commit();
+    }
+}
